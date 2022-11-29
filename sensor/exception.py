83b2e3abbd60a38a2a8cedc1759c346da012902d
Tufail <tufailahmed023@gmail.com>
2022-11-29 17:29:58 +0530

    Updated

--- sensor/exception.py
+++ sensor/exception.py
@@ -3,9 +3,7 @@ import sys,os
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message = "Error occurred python script name [{0}] line number [{1}] error message [{2}]".format(
-        file_name, exc_tb.tb_lineno, str(error)
-    )
+    error_message = "Error occurred python script name [{0}] line number [{1}] error message [{2}]".format(file_name, exc_tb.tb_lineno, str(error))
     return error_message
 
 
@@ -17,4 +15,4 @@ class SensorException(Exception):
             error_message, error_detail=error_detail)
 
     def __str__(self):
-        return self.
+        return self.error_message
