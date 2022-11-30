cd1cce6b76327525359c03bad6b785b1346dcdce
Tufail <tufailahmed023@gmail.com>
2022-11-30 13:05:55 +0530

    test the data ingestion

--- sensor/components/data_ingestion.py
+++ sensor/components/data_ingestion.py
@@ -1,5 +1,5 @@
 from sensor import utils
-from sensor.entity import config_entity
+from sensor.entity import config_entiy
 from sensor.entity import artifact_entity
 from sensor.exception import SensorException
 from sensor.logger import logging
@@ -8,15 +8,15 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-
 class DataIngestion:
-     def __init__(self,data_ingestion_config:config_entity.DataIngestionConfig ):
+    
+    def __init__(self,data_ingestion_config:config_entiy.DataIngestionConfig ):
         try:
             self.data_ingestion_config = data_ingestion_config
         except Exception as e:
-            raise SensorException(e, sys)
+            print(SensorException(e, sys))
 
-     def initiate_data_ingestion(self)->artifact_entity.DataIngestionArtifact:
+    def initiate_data_ingestion(self)->artifact_entity.DataIngestionArtifact:
         try:
             logging.info(f"Exporting collection data as pandas dataframe")
             #Exporting collection data as pandas dataframe
@@ -64,4 +64,4 @@ class DataIngestion:
             return data_ingestion_artifact
 
         except Exception as e:
-            raise SensorException(error_message=e, error_detail=sys) 
+            print(SensorException(error_message=e, error_detail=sys))
---
+++ sensor/main.py
@@ -0,0 +1,10 @@
+from sensor.entity import config_entiy
+from sensor.components.data_ingestion import DataIngestion
+train = config_entiy.TrainingPipelineConfig()
+
+data = config_entiy.DataIngestionConfig(train)
+
+print(data.to_dict())
+
+data_ingestion = DataIngestion(data)
+print(data_ingestion.initiate_data_ingestion())
