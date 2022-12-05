a7d6ad06e6df48b286f15c5d91839355d4cdb144
Tufail <tufailahmed023@gmail.com>
2022-12-05 23:17:51 +0530

    done with model training

--- sensor/main.py
+++ sensor/main.py
@@ -3,12 +3,19 @@ from sensor.components.data_ingestion import DataIngestion
 from sensor.components.data_validation import DataValidation
 from sensor.entity.artifact_entity import DataIngestionArtifact
 from sensor.components.data_transformation import DataTransformation
+from sensor.components.model_trainer import ModelTrainer
+
+
 train = config_entity.TrainingPipelineConfig()
 
-data = config_entity.DataIngestionConfig(train)
+#print(data.to_dict())
+print("=="*20)
+print("=="*20)
+print("=="*20)
 
-print(data.to_dict())
 print("start of dataingestion"*10)
+
+data = config_entity.DataIngestionConfig(train)
 data_ingestion = DataIngestion(data)
 data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
 
@@ -32,8 +39,13 @@ data2 = config_entity.DataTransformationConfig(train)
 obj_tran =  DataTransformation(data2, data_ingestion_artifact = data_ingestion_artifact)
 data_tansformation_artifact = obj_tran.initiate_data_transformation()
 
+print("=="*20)
+print("=="*20)
+print("=="*20)
 
-# import os
-# import pandas as pd
-# base  =  os.path.join(r"C:\Users\KIIT\Desktop\aps-falut-detction\aps-fault-detction\aps_failure_training_set1.csv")
-# pd.read_csv(base)
+print("start of modeltraining"*10)
+
+data3 = config_entity.ModelTrainerConfig(train)
+obj_train = ModelTrainer(data3, data_transformation_artifact = data_tansformation_artifact)
+model_trainer_artifact = obj_train.initiate_model_trainer()
+
