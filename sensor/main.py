76bc56ae4d55944639cf53faa1aef7f59694e856
Tufail <tufailahmed023@gmail.com>
2022-12-11 11:49:30 +0530

    done with batch prediction

--- sensor/main.py
+++ sensor/main.py
@@ -1,3 +1,4 @@
 from sensor.pipeline.training_pipeline import start_training_pipeline
+from sensor.pipeline.batch_prediction import start_batch_prediction
 
-start_training_pipeline()
+start_batch_prediction(r"C:\Users\KIIT\Desktop\aps-falut-detction\aps-fault-detction\sensor\artifact\12102022__231319\data_ingestion\dataset\test.csv")
